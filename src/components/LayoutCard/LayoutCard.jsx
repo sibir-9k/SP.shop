e8d3463da58f42ec3ffcb8973bccee31e39996a7
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-03-31 18:57:34 +0300

    the status for the component is transferred

--- src/components/LayoutCard/LayoutCard.jsx
+++ src/components/LayoutCard/LayoutCard.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import bucketBuy from '../../assets/img/bucketWhite.svg';
-import testImg from '../../assets/img/newBalance574.jpg'
-
-import "./LayoutCard.scss"
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import bucketBuy from '../../assets/img/bucketWhite.svg';
+
+import './LayoutCard.scss';
 
 export const LayoutCard = () => {
-  return (
-    <div className="layout-card">
-      <Link to={'/'} className="link-back">Back in Catalog</Link >
-      <div className="layout-card__block card-block">
-        <img src={testImg} alt="картинка" />
-        <h3 className="card-block__name">New Balance 574 Vintage Brights</h3>
-        <p className="card-block__modelNumber">Item model number: MT91547</p>
-        <div className="card-block__buy-price">
-          <button className="buy">
-            <img src={bucketBuy} alt="" />
-          </button>
-          <span className="price">$ 650</span>
-        </div>
-      </div>
-    </div >
-  );
+	const productItem = useSelector((state) => state.show);
+
+	const { cardName, img, cardPrice } = productItem;
+
+	return (
+		<div className="layout-card">
+			<Link to={'/'} className="link-back">
+				Back in Catalog
+			</Link>
+			<div className="layout-card__block card-block">
+				<img src={img} alt="картинка" />
+				<h3 className="card-block__name">{cardName}</h3>
+				<p className="card-block__modelNumber">Item model number: MT91547</p>
+				<div className="card-block__buy-price">
+					<button className="buy">
+						<img src={bucketBuy} alt="" />
+					</button>
+					<span className="price">$ {cardPrice}</span>
+				</div>
+			</div>
+		</div>
+	);
 };
