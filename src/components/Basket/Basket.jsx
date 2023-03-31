ec8749833e103db3f369a43824c101c555fc0e1e
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-03-31 16:43:55 +0300

    Added condition when there is no product in the shopping basket

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -8,26 +8,30 @@ import './Basket.scss';
 
 export const Basket = ({ active, setActive }) => {
 
-	const productArray = useSelector((state) => state.card.listItems);
+  const productArray = useSelector((state) => state.card.listItems);
 
-	return (
-		<div className={active ? 'basket active' : 'basket'} onClick={() => setActive(false)}>
-			<div className="basket__content content" onClick={(e) => e.stopPropagation()}>
-				<button className="active__close" onClick={() => setActive(false)}>
-					<img src={Cross} alt="" />
-				</button>
-				<div className="active__product">
-					<div className="active-list__name">
-						<strong>My basket</strong>
-					</div>
-					<ul className="active-product__list">
-						{productArray.map((item) => {
-							return <MiniCard {...item} key={item.id} />;
-						})}
-					</ul>
-				</div>
-				<AmountCheck />
-			</div>
-		</div>
-	);
+  return (
+    <div className={active ? 'basket active' : 'basket'} onClick={() => setActive(false)}>
+      <div className="basket__content content" onClick={(e) => e.stopPropagation()}>
+        <button className="active__close" onClick={() => setActive(false)}>
+          <img src={Cross} alt="" />
+        </button>
+        <div className="active__product">
+          <div className="active-list__name">
+            <strong>My basket</strong>
+          </div>
+          {
+            productArray.length < 1
+              ? <h2 style={{marginBottom: '40px'}}>Product not yet added to cart</h2>
+              : <ul className="active-product__list">
+                {productArray.map((item) => {
+                  return <MiniCard {...item} key={item.id} />;
+                })}
+              </ul>
+          }
+        </div>
+        <AmountCheck />
+      </div>
+    </div>
+  );
 };
