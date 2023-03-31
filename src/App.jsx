d282c8037e0860bb340e2baa34e6355f9135dd83
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-03-31 16:49:17 +0300

    Basic page routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
 import { CardList } from './components/CardList/CardList'
 import { Header } from './components/Header/Header'
 import './App.css'
+import { LayoutCard } from './components/LayoutCard/LayoutCard'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CardList />
+      <Routes>
+        <Route path="/" element={<CardList />} />
+        <Route path="/product" element={<LayoutCard />} />
+      </Routes>
     </div>
   )
 }
 
 export default App
+
+
