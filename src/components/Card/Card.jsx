95c1284ed013724129eaecdd5252ae0ed3f364ec
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-03-31 16:47:04 +0300

    temporarily added a link to the card name using Link from react router

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,37 +4,39 @@ import { addCardInBusket } from '../../app/features/cardSlice';
 
 import bucketBuy from '../../assets/img/bucketWhite.svg';
 import './Card.scss';
+import { Link } from 'react-router-dom';
 
 export const Card = ({ id, title, imgUrl, price }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const addCard = () => {
-		dispatch(
-			addCardInBusket({
-				name: title,
-				price,
-				id,
-				img: imgUrl,
-			})
-		);
-	};
+  const addCard = () => {
+    dispatch(
+      addCardInBusket({
+        name: title,
+        price,
+        id,
+        img: imgUrl,
+      })
+    );
+  };
 
-	return (
-		<div className="card">
-			<div className="card__img">
-				<img src={imgUrl} alt="" />
-			</div>
-			<div className="card__info">
-				<div className="card__info-productName">
-					<strong>{title}</strong>
-				</div>
-				<div className="card__info-buy-price">
-					<button className="buy" onClick={addCard}>
-						<img src={bucketBuy} alt="" />
-					</button>
-					<span className="price">$ {price}</span>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="card">
+      <div className="card__img">
+        <img src={imgUrl} alt="" />
+      </div>
+      <div className="card__info">
+        <div className="card__info-productName">
+          <Link to={'product'}>{title}</Link>
+        </div>
+        <div className="card__info-buy-price">
+          <button className="buy" onClick={addCard}>
+            <img src={bucketBuy} alt="" />
+          </button>
+          <span className="price">$ {price}</span>
+        </div>
+      </div>
+    </div>
+
+  );
 };
