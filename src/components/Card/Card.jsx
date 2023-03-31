9b19aa302914e39b8b2ec02fc7e012581017ec09
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-03-31 18:54:02 +0300

    added new func

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,6 +5,7 @@ import { addCardInBusket } from '../../app/features/cardSlice';
 import bucketBuy from '../../assets/img/bucketWhite.svg';
 import './Card.scss';
 import { Link } from 'react-router-dom';
+import { showCardLayout } from '../../app/features/showCardSlice';
 
 export const Card = ({ id, title, imgUrl, price }) => {
   const dispatch = useDispatch();
@@ -20,6 +21,17 @@ export const Card = ({ id, title, imgUrl, price }) => {
     );
   };
 
+  const showCard = () => {
+    dispatch(
+      showCardLayout({
+        name: title,
+        price,
+        id,
+        img: imgUrl,
+      })
+    );
+  };
+
   return (
     <div className="card">
       <div className="card__img">
@@ -27,7 +39,7 @@ export const Card = ({ id, title, imgUrl, price }) => {
       </div>
       <div className="card__info">
         <div className="card__info-productName">
-          <Link to={'product'}>{title}</Link>
+          <Link to={'product'} onClick={showCard}>{title}</Link>
         </div>
         <div className="card__info-buy-price">
           <button className="buy" onClick={addCard}>
